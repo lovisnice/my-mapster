c21919f16ac5c0521dbc9a2f63c1e3e5863c0702
lovisnice <vovaprorok29@gmail.com>
2024-02-20 23:36:03 +0200

    pagination & search

--- src/category/list/CategoryListPage.tsx
+++ src/category/list/CategoryListPage.tsx
@@ -1,12 +1,11 @@
-import {Button, Popconfirm, Table} from "antd";
-import {Link} from "react-router-dom";
-import {ColumnsType} from "antd/es/table";
-
-import {EditOutlined, DeleteOutlined} from '@ant-design/icons';
-import {useEffect, useState} from "react";
-import {APP_ENV} from "../../env";
+import { Button, Col, Input, Popconfirm, Row, Table } from "antd";
+import { Link } from "react-router-dom";
+import { ColumnsType } from "antd/es/table";
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { useEffect, useState } from "react";
+import { APP_ENV } from "../../env";
 import http_common from "../../http_common.ts";
-import {ICategoryItem} from "../create/types.ts";
+import { ICategoryItem } from "../create/types.ts";
 
 const CategoryListPage = () => {
     const imgURL = APP_ENV.BASE_URL + "/uploading/150_";
@@ -28,7 +27,7 @@ const CategoryListPage = () => {
             title: 'Image',
             dataIndex: 'image',
             render: (imageName: string) => (
-                <img src={`${imgURL}${imageName}`} alt="Category Image"/>
+                <img src={`${imgURL}${imageName}`} alt="Category Image" />
             ),
         },
         {
@@ -36,73 +35,107 @@ const CategoryListPage = () => {
             dataIndex: 'edit',
             render: (_, record) => (
                 <Link to={`/category/edit/${record.id}`}>
-                    <Button type="primary" icon={<EditOutlined/>}>
+                    <Button type="primary" icon={<EditOutlined />}>
                         Змінити
                     </Button>
                 </Link>
-
             ),
         },
         {
             title: 'Delete',
             dataIndex: 'delete',
             render: (_, record) => (
-
                 <Popconfirm
                     title="Are you sure to delete this category?"
                     onConfirm={() => handleDelete(record.id)}
                     okText="Yes"
                     cancelText="No"
                 >
-                    <Button icon={<DeleteOutlined/>}>
+                    <Button icon={<DeleteOutlined />}>
                         Delete
                     </Button>
                 </Popconfirm>
-
             ),
         },
     ];
 
     const [data, setData] = useState<ICategoryItem[]>([]);
+    const [pagination, setPagination] = useState({ current: 1, pageSize: 5 });
+
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await http_common.get("/api/categories");
-                console.log("response.data", response.data)
-                setData(response.data);
-            } catch (error) {
-                console.error('Error fetching categories:', error);
-            }
-        };
-
         fetchData();
-    }, []);
+    }, [pagination]);
+
+    const fetchData = async () => {
+        try {
+            const response = await http_common.get("/api/categories", {
+                params: { page: pagination.current, limit: pagination.pageSize }
+            });
+            setData(response.data);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        }
+    };
 
     const handleDelete = async (categoryId: number) => {
         try {
             await http_common.delete(`/api/categories/${categoryId}`);
-            setData(data.filter(x => x.id != categoryId));
+            setData(data.filter(x => x.id !== categoryId));
         } catch (error) {
-            throw new Error(`Error: ${error}`);
+            console.error('Error deleting category:', error);
         }
+    };
+
+    const handleTableChange = (pagination: any) => {
+        setPagination(pagination);
+    };
+
+    const handleSearchChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        try {
+            const response = await http_common.get(`/api/categories/search?query=${e.target.value}`);
+            setData(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        }
+
     };
 
     return (
         <>
-            <h1>Список категорій</h1>
+            <Row gutter={[16, 16]} align="middle">
+                <Col flex="auto">
+                    <h1 style={{ marginBottom: 16 }}>Список категорій</h1>
+                </Col>
+                <Col flex="200px">
+                    <Input.Search
+                        placeholder="Search category"
+                        enterButton
+                        onChange={handleSearchChange}
+                        //value={searchText}
+                        style={{ width: '100%' }}
+                    />
+                </Col>
+            </Row>
 
-
-            <Table columns={columns} rowKey={"id"} dataSource={data} size="middle"/>
-
+            <Table
+                columns={columns}
+                rowKey={"id"}
+                dataSource={data}
+                size="middle"
+                pagination={pagination}
+                onChange={handleTableChange}
+            />
 
             <Link to={"/category/create"}>
-                <Button type="primary" style={{margin: '5px'}}>
+                <Button type="primary" style={{ margin: '5px' }}>
                     ADD +
                 </Button>
             </Link>
         </>
     );
-}
+};
 
 export default CategoryListPage;
